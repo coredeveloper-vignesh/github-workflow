934f8f00cd8d59360a9dcac82ad7999ba04d4584
vignesh-getstertech <vignesh.k@getster.tech>
2024-02-27 15:02:30 +0530

    Src Updated.

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  deploy_type: 'production',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  baseURL: '',
-  deploy_type: 'developing',
-};
